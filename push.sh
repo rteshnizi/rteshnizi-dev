6e326500d43d03de191d735a90eebfc2b12013fb
Reza Teshnizi <rht1369@yahoo.com>
2019-02-28 00:09:54 -0600

    chore(travis): Use a fake email for travis to differentiate from me

--- push.sh
+++ push.sh
@@ -12,7 +12,7 @@ prepare_output() {
 }
 
 setup_git() {
-	git config --global user.email "rht1369@yahoo.com"
+	git config --global user.email "travis@travis-ci.org"
 	git config --global user.name "Travis CI (on behalf of Reza Teshnizi)"
 }
 
