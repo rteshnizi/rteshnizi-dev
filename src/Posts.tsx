68e30244ec45cb39449d0ca24870318b44ee2722
Reza Teshnizi <rht1369@yahoo.com>
2019-09-24 02:05:34 -0500

    chore(all): Fix ESLint errors

--- src/Posts.tsx
+++ src/Posts.tsx
@@ -1,6 +1,6 @@
 import * as React from 'react';
 import * as Router from 'react-router-dom';
-import * as Mui from '@material-ui/core';
+// import * as Mui from '@material-ui/core';
 import ComponentBase from './Base/ComponentBase';
 
 class Posts extends ComponentBase<Router.RouteComponentProps> {
