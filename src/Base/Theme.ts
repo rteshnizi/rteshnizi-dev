386fe5ac3b47986ecaae1fe9d38d005527ff74bf
Reza Teshnizi <rht1369@yahoo.com>
2019-11-17 22:43:48 -0600

    feat(Description): Update

--- src/Base/Theme.ts
+++ src/Base/Theme.ts
@@ -1,6 +1,9 @@
 import { createMuiTheme } from '@material-ui/core';
 
 export default createMuiTheme({
+	palette: {
+		type: 'light',
+	},
 	overrides: {
 		MuiLink: {
 			root: {
--- src/Description.tsx
+++ src/Description.tsx
@@ -1,4 +1,5 @@
 import * as React from 'react';
+import * as Mui from '@material-ui/core';
 import ComponentBase from './Base/ComponentBase';
 
 export class Description extends ComponentBase {
@@ -8,9 +9,22 @@ export class Description extends ComponentBase {
 
 	public render(): JSX.Element {
 		return (
-			<p>
-				More Content to be added. For now, please follow me on Medium.
-			</p>
+			<Mui.Grid container={true} spacing={2} style={{ flexGrow: 1 }}>
+				<Mui.Grid item={true} xs={1}>&nbsp;</Mui.Grid>
+				<Mui.Grid item={true} xs={10}>
+					<p>
+						Reza Teshnizi is a PhD student under the supervision of <a href="http://robotics.cs.tamu.edu/dshell/">Dr. Dylan Shell</a> in <a href="https://engineering.tamu.edu/cse/index.html">Computer Science</a> at <a href="https://tamu.edu/">Texas A&amp;M University</a>.<br />
+						His research interest involves Artificial Intelligence and Machine Learning.
+						He enjoys studying Motion Planning and Reinforcement Learning.
+						He has studied motion planning algorithms for tethered robots.
+
+						Reza received his BSc in 2012 from <a href="https://aut.ac.ir/">Amirkabir University of Technology (Tehran Polytechnic)</a> in Computer Engineering.
+						In 2015, he defended his master's thesis titled <b>"Motion Planning for a Tethered Mobile Robot"</b> to receive MS in Computer Science from Texas A&amp;M University.
+						He has experience as a Software Engineer and manager in FactSet Research Systems, inc.
+					</p>
+				</Mui.Grid>
+				<Mui.Grid item={true} xs={1}>&nbsp;</Mui.Grid>
+			</Mui.Grid>
 		);
 	}
 }
