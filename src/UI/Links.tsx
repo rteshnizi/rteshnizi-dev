46c03ede03008a7b220d7bc15780b039c0d7fa15
Reza Teshnizi <rht1369@yahoo.com>
2021-02-25 14:13:08 -0600

    feat(Links): Add ORCiD

--- src/UI/Links.tsx
+++ src/UI/Links.tsx
@@ -1,7 +1,7 @@
 import * as React from 'react';
 import * as Mui from '@material-ui/core';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faLinkedin, faGithub } from '@fortawesome/free-brands-svg-icons';
+import { faLinkedin, faGithub, faOrcid } from '@fortawesome/free-brands-svg-icons';
 import { faGraduationCap, faEnvelope } from '@fortawesome/free-solid-svg-icons';
 import ComponentBase from '../Base/ComponentBase';
 import Theme from '../Base/Theme';
@@ -83,6 +83,13 @@ export class Links extends ComponentBase<{}, State> {
 							</Mui.Grid>
 							<Mui.Grid item={true}>
 								<IconLink
+									icon={<FontAwesomeIcon icon={faOrcid} size="lg" />}
+									label="ORCiD"
+									url="https://orcid.org/0000-0001-8879-7146"
+								/>
+							</Mui.Grid>
+							<Mui.Grid item={true}>
+								<IconLink
 									icon={<Mui.Avatar src="https://logo.clearbit.com/ieee.org?greyscale=true" style={{ width: 30, height: 30 }} />}
 									label="IEEE Xplore Profile"
 									url="https://ieeexplore.ieee.org/author/37085347277"
@@ -91,14 +98,14 @@ export class Links extends ComponentBase<{}, State> {
 							<Mui.Grid item={true}>
 								<IconLink
 									icon={<FontAwesomeIcon icon={faLinkedin} size="lg" />}
-									label="Linkedin Profile"
+									label="Linkedin"
 									url="https://www.linkedin.com/in/rteshnizi/"
 								/>
 							</Mui.Grid>
 							<Mui.Grid item={true}>
 								<IconLink
 									icon={<FontAwesomeIcon icon={faGithub} size="lg" />}
-									label="Github Profile"
+									label="Github"
 									url="https://github.com/rteshnizi"
 								/>
 							</Mui.Grid>
